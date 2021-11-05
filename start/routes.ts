1557e35f237bcb58a0ce7b3f679ff257ec4d843c
ekpono <ekponoambrose@gmail.com>
2021-11-05 01:43:36 +0100

    base url

--- start/routes.ts
+++ start/routes.ts
@@ -1,9 +1,17 @@
 import Route from '@ioc:Adonis/Core/Route'
 
+Route.get('/', (ctx) => {
+  ctx.response.send({message: 'Connection successful'})
+})
+
 Route.group(() => {
 
-  // Route.post('login',     'AuthController.login')
-  // Route.post('register', 'AuthController.register')
+  Route.get('/', (ctx) => {
+    ctx.response.send({message: 'api connected'})
+  })
+
+  Route.post('login',     'AuthController.login')
+  Route.post('register', 'AuthController.register')
 
   Route.group(() => {
     Route.resource('products',             'ProductController')
